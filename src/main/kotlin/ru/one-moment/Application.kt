1f789b104c973ff934b37ddb9fb75d4a2a356e7f
StanislavGro <stanislav.skii2000@mail.ru>
2023-10-13 18:07:39 +0700

    [commands] Добавил менюшку снизу

--- src/main/kotlin/ru/one-moment/Application.kt
+++ src/main/kotlin/ru/one-moment/Application.kt
@@ -1,8 +1,20 @@
 package ru.`one-moment`
 
+import com.github.kotlintelegrambot.entities.BotCommand
 import ru.`one-moment`.bot.OneMomentBot
 
 fun main(args: Array<String>) {
+//    args.forEach {
+//        println(it)
+//    }
+
     val oneMomentBot = OneMomentBot().createBot()
+    oneMomentBot.setMyCommands(
+        listOf(
+            BotCommand("/start", "Запуск бота"),
+            BotCommand("/teachers", "Посмотреть всех преподавателей"),
+            BotCommand("/help", "Список команд")
+        )
+    )
     oneMomentBot.startPolling()
 }
--- src/main/kotlin/ru/one-moment/bot/OneMomentBot.kt
+++ src/main/kotlin/ru/one-moment/bot/OneMomentBot.kt
@@ -3,14 +3,14 @@ package ru.`one-moment`.bot
 import com.github.kotlintelegrambot.Bot
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
-import com.github.kotlintelegrambot.dispatcher.*
-import com.github.kotlintelegrambot.entities.*
+import com.github.kotlintelegrambot.dispatcher.Dispatcher
+import com.github.kotlintelegrambot.dispatcher.callbackQuery
+import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.entities.ChatId
+import com.github.kotlintelegrambot.entities.InlineKeyboardMarkup
+import com.github.kotlintelegrambot.entities.ParseMode
 import com.github.kotlintelegrambot.entities.keyboard.InlineKeyboardButton
-import com.github.kotlintelegrambot.extensions.filters.Filter
 import com.github.kotlintelegrambot.logging.LogLevel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 private const val BOT_ANSWER_TIMEOUT = 30
 private const val BOT_TOKEN = "6554932402:AAEYN8oYHnUmp4yWY5mnSFjkr7MnmkiVCqE"
@@ -37,11 +37,13 @@ class OneMomentBot {
     }
 
     private fun Dispatcher.setUpCommands() {
+
         command("start") {
             _chatId = ChatId.fromId(message.chat.id)
+
             bot.sendMessage(
                 chatId = chatId,
-                text = "Привет! Прежде чем начать, давай зарегистрируемся /login"
+                text = "Привет! Добро пожаловать"
             )
         }
 
@@ -56,6 +58,23 @@ class OneMomentBot {
                 parseMode = ParseMode.MARKDOWN_V2
             )
         }
+
+        command("help") {
+            _chatId = ChatId.fromId(message.chat.id)
+            messageId = message.messageId
+
+            bot.sendMessage(
+                chatId = chatId,
+                text =
+                    """
+                        Список команд:
+                        /start - запустить бота
+                        /teachers - посмотреть всех учителей
+                        
+                        /help - посмотреть все команды
+                    """.trimIndent()
+            )
+        }
     }
 
     private fun Dispatcher.setUpCallbacks() {
@@ -66,11 +85,12 @@ class OneMomentBot {
                 teacherId--
             }
 
-            bot.sendMessage(
+            bot.editMessageText(
                 chatId = chatId,
-                text = teacherList[teacherId],
+                messageId = messageId?.plus(1L),
                 replyMarkup = inlineKeyboard,
-                parseMode = ParseMode.MARKDOWN_V2
+                parseMode = ParseMode.MARKDOWN_V2,
+                text = teacherList[teacherId],
             )
         }
 
@@ -81,11 +101,12 @@ class OneMomentBot {
                 teacherId++
             }
 
-            bot.sendMessage(
+            bot.editMessageText(
                 chatId = chatId,
-                text = teacherList[teacherId],
+                messageId = messageId?.plus(1L),
                 replyMarkup = inlineKeyboard,
-                parseMode = ParseMode.MARKDOWN_V2
+                parseMode = ParseMode.MARKDOWN_V2,
+                text = teacherList[teacherId],
             )
         }
     }
@@ -103,10 +124,12 @@ class OneMomentBot {
             listOf(
                 InlineKeyboardButton.CallbackData(
                     text = "⬅️",
+//                    text = "<",
                     callbackData = "prevTeacher"
                 ),
                 InlineKeyboardButton.CallbackData(
                     text = "➡️",
+//                    text = ">",
                     callbackData = "nextTeacher"
                 )
             )
